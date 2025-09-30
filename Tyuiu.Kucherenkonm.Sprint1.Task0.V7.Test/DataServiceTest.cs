84eef8e9f750529973c507da06f369da15bf86be
wyrtnvll <date1818@yandex.ru>
2025-09-30 21:37:32 +0500

    Add project files.

---
+++ Tyuiu.Kucherenkonm.Sprint1.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.Kucherenkonm.Sprint1.Task0.V7.Lib;
+namespace Tyuiu.Kucherenkonm.Sprint1.Task0.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ Tyuiu.Kucherenkonm.Sprint1.Task0.V7/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.Kucherenkonm.Sprint1.Task0.V7.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнила: Кучеренко Н. М. | ИИПб - 25 - 1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #7                                                              *");
+        Console.WriteLine("* Выполнила: Кучеренко Надежда Михайловна | ИИПб - 25 - 1                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать консольную программу на С#, которая вычисляет выражение        *");
+        Console.WriteLine("*         24/(6*2)-24/6/4 и печатает результат на экране                  *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("*  24/(6*2)-24/6/4                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine(ds.Calculate());
+        Console.ReadKey();
+    }
+}
