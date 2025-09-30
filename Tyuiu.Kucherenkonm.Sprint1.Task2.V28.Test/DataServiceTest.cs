f97926bef4e98180a1c68e11ee0766501d4a34aa
wyrtnvll <date1818@yandex.ru>
2025-10-01 00:43:21 +0500

    Task2

---
+++ Tyuiu.Kucherenkonm.Sprint1.Task2.V28.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.Kucherenkonm.Sprint1.Task2.V28.Lib;
+namespace Tyuiu.Kucherenkonm.Sprint1.Task2.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            var ds = new DataService();
+            int x = 2;
+            Assert.AreEqual(275, ds.ConvertCelsiusToKelvin(x));
+        }
+    }
+}
--- Tyuiu.Kucherenkonm.Sprint1.Task2.V28/Program.cs
+++ Tyuiu.Kucherenkonm.Sprint1.Task2.V28/Program.cs
@@ -1,4 +1,4 @@
-﻿using Tyuiu.Kucherenkonm.Sprint1.Task1.V4.Lib;
+﻿using Tyuiu.Kucherenkonm.Sprint1.Task2.V28.Lib;
 internal class Program
 {
     static void Main(string[] args)
@@ -9,30 +9,27 @@ internal class Program
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* Спринт #1                                                               *");
         Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
-        Console.WriteLine("* Задание #1                                                              *");
-        Console.WriteLine("* Вариант #4                                                              *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #28                                                              *");
         Console.WriteLine("* Выполнила: Кучеренко Надежда Михайловна | ИИПб - 25 - 1                 *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* УСЛОВИЕ:                                                                *");
-        Console.WriteLine("* Написать консольную программу на С#, которая вычисляет по формуле       *");
-        Console.WriteLine("*          (x+y)/x*y и печатает результат на экране                       *");
+        Console.WriteLine("*    Известна температура в градусах Цельсия. Перевести                   *");
+        Console.WriteLine("*          температуру в градусы Кельвина                                 *");
         Console.WriteLine("*                                                                         *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
         Console.WriteLine("***************************************************************************");
 
-        double x, y;
-        Console.WriteLine("Введите значение x:");
-        x = Convert.ToDouble(Console.ReadLine());
-
-        Console.WriteLine("Введите значение y:");
-        y = Convert.ToDouble(Console.ReadLine());   
+        int x;
+        Console.WriteLine("Введите значение x в градусах Цельсия: ");
+        x = Convert.ToInt32(Console.ReadLine());
 
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
         Console.WriteLine("***************************************************************************");
-
-        Console.WriteLine(ds.Calculate(x, y));
+        Console.WriteLine("Значение х в градусах Кельвина: " + ds.ConvertCelsiusToKelvin(x));
         Console.ReadKey();
+
     }
 }
