242becf491e5beed92326965e327605200c4fa15
wyrtnvll <date1818@yandex.ru>
2025-10-02 01:55:24 +0500

    Task7

--- Tyuiu.Kucherenkonm.Sprint1.Task7.V14.Lib/DataService.cs
+++ Tyuiu.Kucherenkonm.Sprint1.Task7.V14.Lib/DataService.cs
@@ -5,12 +5,8 @@ namespace Tyuiu.Kucherenkonm.Sprint1.Task7.V14.Lib
     {
         public double Calculate(double x, double y)
         {
-            double x1 = Math.Pow(2, -x);
-            double x2 = (5*Math.Pow(x, 2))/(3*Math.Pow(x,3));
-            double x3 = Math.Cos(Math.Pow(x,2));
-            double x4 = Math.Sin(2 * x * y);
-            double res = x1 + x2 - x3 + x4;
-            return Math.Round(res, 3, MidpointRounding.ToZero);
+            double res = Math.Pow(2, -x) + ((5 * Math.Pow(x, 2)) / (3 * Math.Pow(x, 3))) - Math.Cos(Math.Pow(x, 2)) + Math.Sin(2 * x * y);
+            return Math.Round(res, 3);
         }
     }
 }
