bee4c7653df7ef69a0ad0d9cf8aafd529475f38c
wyrtnvll <date1818@yandex.ru>
2025-10-01 20:43:25 +0500

    Task3

---
+++ Tyuiu.Kucherenkonm.Sprint1.Task3.V4.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.Kucherenkonm.Sprint1.Task3.V4.Lib;
+namespace Tyuiu.Kucherenkonm.Sprint1.Task3.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            var ds = new DataService();
+            double x = 2.75;
+            double y = 0.5;
+            int z = 7;
+            var res = ds.PurchaseAmount(x,y,z);
+            Assert.AreEqual(22.75,res);
+        }
+    }
+}
---
+++ Tyuiu.Kucherenkonm.Sprint1.Task3.V4/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.Kucherenkonm.Sprint1.Task3.V4.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнила: Кучеренко Н. М. | ИИПб - 25 - 1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #4                                                              *");
+        Console.WriteLine("* Выполнила: Кучеренко Надежда Михайловна | ИИПб - 25 - 1                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*       Написать программу вычисления стоимости покупки, состоящей из     *");
+        Console.WriteLine("*         нескольких тетрадей и такого же количества обложек к ним        *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        double PriceNote = 2.75;
+        double PriceCover = 0.5;
+        int quantity = 7;
+        Console.WriteLine(" Цена тетради (руб): " + PriceNote);
+        Console.WriteLine(" Цена обложки (руб): " + PriceCover);
+        Console.WriteLine(" Количество комплектов (шт): " + quantity);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(" Стоимость покупки составит: " + ds.PurchaseAmount(PriceNote, PriceCover, quantity));
+        Console.ReadKey();
+
+
+        Console.ReadKey();
+
+    }
+}
