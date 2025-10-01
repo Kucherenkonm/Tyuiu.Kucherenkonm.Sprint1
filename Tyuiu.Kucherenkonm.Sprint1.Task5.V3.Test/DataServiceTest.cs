7e8f710a7ef5816388659edc012d9404d7c2e744
wyrtnvll <date1818@yandex.ru>
2025-10-01 22:06:07 +0500

    Task5

---
+++ Tyuiu.Kucherenkonm.Sprint1.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.Kucherenkonm.Sprint1.Task5.V3.Lib;
+namespace Tyuiu.Kucherenkonm.Sprint1.Task5.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            var ds = new DataService();
+
+            int k = 130985;
+            int res = ds.Calculate(k);
+            int result = Convert.ToInt32(res);
+            int h = 9;
+            Assert.AreEqual(h, result);
+
+        }
+    }
+}
---
+++ Tyuiu.Kucherenkonm.Sprint1.Task5.V3/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.Kucherenkonm.Sprint1.Task5.V3.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнила: Кучеренко Н. М. | ИИПб - 25 - 1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #3                                                              *");
+        Console.WriteLine("* Выполнила: Кучеренко Надежда Михайловна | ИИПб - 25 - 1                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*            Написать программу, которая решает следующую задачу:         *");
+        Console.WriteLine("*  Присвоить переменной h третью от конца цифру в записи положительного   *");
+        Console.WriteLine("*                         целого числа k                                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int h, k;
+
+        Console.WriteLine("Введите значение k:");
+        k = Convert.ToInt32(Console.ReadLine());
+
+        h = Convert.ToInt32(ds.Calculate(k));
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Третья от конца цифра в записи целого положительного числа к: " + h);
+        Console.ReadKey();
+
+    }
+}
